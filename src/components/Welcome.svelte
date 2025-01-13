3f1fb2a4d57acca2caf02bf91e96eeb5b445a605
zrz616 <zrz616@gmail.com>
2025-01-13 15:02:12 +0800

    feat: 添加 Svelte 和 Tailwind 集成，重构 Welcome 组件为 Svelte 组件

---
+++ src/components/Welcome.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Modal from './modal.svelte';
+  let count = 0;
+  let visible = false;
+  let doubled = 0;
+  $: doubled = count * 2;
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen">
+  <h1 class="text-2xl font-bold mb-4">Welcome to <span class="text-red-400 text-3xl uppercase">Svelte</span></h1>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-light text-xs py-2 px-4 rounded-full mb-2" on:click={() => {
+    count++;
+    if (count % 10 === 0) {
+      count = 0;
+      console.log('visible', visible);
+      visible = true;
+    }
+  }}>
+      count: {count}
+  </button>
+  
+  <p class="text-lg">Doubled: {doubled}</p>
+  <Modal bind:visible={visible} />
+</div>
---
+++ src/components/modal.svelte
@@ -0,0 +1,31 @@
+<!-- svelte-ignore options_missing_custom_element -->
+<!-- a modal for edit order -->
+<script lang="ts">
+  export let visible = false;
+  let orderCopy = {
+    "name": "sam",
+    "description": "sam is bigger than life",
+    "price": 0
+  }; 
+
+  function saveOrder() {
+    visible = false;
+  }
+
+  function close() {
+    visible = false;
+  }
+</script>
+
+{#if visible}
+<div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+  <div class="bg-white p-4 rounded-lg w-1/2">
+    <h2 class="text-xl font-bold mb-4">Edit Order</h2>
+    <input type="text" class="border border-gray-300 p-2 rounded-lg w-full mb-4" bind:value={orderCopy.name} placeholder="Name" />
+    <input type="text" class="border border-gray-300 p-2 rounded-lg w-full mb-4" bind:value={orderCopy.description} placeholder="Description" />
+    <input type="number" class="border border-gray-300 p-2 rounded-lg w-full mb-4" bind:value={orderCopy.price} placeholder="Price" />
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-light text-xs py-2 px-4 rounded-full mb-2" onclick={saveOrder}>Save</button>
+    <button class="bg-red-500 hover:bg-red-700 text-white font-light text-xs py-2 px-4 rounded-full" onclick={close}>Close</button>
+  </div>
+</div>
+{/if}
